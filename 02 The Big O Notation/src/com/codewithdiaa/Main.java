3885f840f7368997ce367485cab9563977ec0893
Diaa Shalabi <diaamshalabi@gmail.com>
2022-02-05 10:53:09 +0300

    Add 02 big-o notation outline & code

---
+++ 02 The Big O Notation/src/com/codewithdiaa/Main.java
@@ -0,0 +1,33 @@
+package com.codewithdiaa;
+
+public class Main {
+    public static void logO1(int[] numbers) {
+        // O(1)
+        System.out.println(numbers[0]); // O(1)
+    }
+
+    public static void logOn(int[] numbers) {
+        // O(n)
+        System.out.println("F"); // O(1)
+        for (int number : numbers)  // O(n)
+            System.out.println(number);
+//        for (String name : names) // O(n)
+//            System.out.println(name);
+        System.out.println("E"); // O(1)
+    }
+
+    public static void logOn2(int[] numbers) {
+        // O(n^2)
+        for (int number : numbers) // O(n)
+        {
+            System.out.println(number);
+            for (int number1 : numbers) // O(n^2) X
+                System.out.println(number1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        logOn2(numbers);
+    }
+}
