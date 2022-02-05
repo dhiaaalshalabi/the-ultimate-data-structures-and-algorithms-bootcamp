0e870efa4ccf95bf3fed1df2892a0bc8f28cf153
Diaa Shalabi <diaamshalabi@gmail.com>
2022-02-05 10:51:04 +0300

    commit

---
+++ 03 The Big O Notation/src/com/codewithdiaa/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int numbers[] = new int[3];
+    }
+
+    public void greet(String[] names) {
+        // O(1) space
+        String[] copy = new String[names.length];
+        // O(n) space
+        for (String name:names)
+            System.out.println("Hi "+name);
+    }
+}
